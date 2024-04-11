f97edd81f705ed4e20b4ca755c2c26690f6bf4b5
WenChen2005 <166739243+WenChen2005@users.noreply.github.com>
2024-04-11 14:58:08 -0400

    Create main.py
    add the decode function.

---
+++ main.py
@@ -0,0 +1,31 @@
+def menu():
+    print()
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Exit")
+
+def encode(password):
+    encode_password = []
+    for digits in password:
+        encode_password = str(int(digits) + 3)
+
+    return encode_password
+
+def decode(password):
+    pass
+
+if __name__ == '__main__':
+    while True:
+        menu()
+        choice = input("Please enter an option: ")
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            encode_password = encode(password)
+            print("Your password has been encoded and stored!")
+            print(encode_password)
+        if choice == "2":
+            print(f"The encoded password is", {encode_password}, "and the original password is", {decode})
+        if choice == "3":
+            break
